e86a73e6cc9866efa39ef295043bdb2b31305df9
David Kuehn <dckuehn8@gmail.com>
2021-08-03 13:57:35 -0500

    Renames or removes default weather classes to Pipeline concepts.

---
+++ SportradarApiChallenge/SportradarApiChallenge/Controllers/Pipeline.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportradarApiChallenge.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PipelineController : ControllerBase
+    {
+        private readonly ILogger<PipelineController> _logger;
+
+        public PipelineController(ILogger<PipelineController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("Teams")]
+        public ActionResult GetTeam(int teamId, int seasonYear)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("Player")]
+        public ActionResult GetPlayer(int playerId, int seasonYear)
+        {
+            return Ok();
+        }
+    }
+}
