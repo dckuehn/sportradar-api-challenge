e5a3bed22757ad94a8d4e78089a657695c38b4af
David Kuehn <dckuehn8@gmail.com>
2021-08-04 10:50:50 -0500

    Renames PipelineController and injects TeamService

--- SportradarApiChallenge/SportradarApiChallenge/Controllers/PipelineController.cs
+++ SportradarApiChallenge/SportradarApiChallenge/Controllers/PipelineController.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using SportradarApiChallenge.Services.Interfaces;
 
 namespace SportradarApiChallenge.Controllers
 {
@@ -12,16 +9,20 @@ namespace SportradarApiChallenge.Controllers
     public class PipelineController : ControllerBase
     {
         private readonly ILogger<PipelineController> _logger;
+        private readonly ITeamService _teamService;
 
-        public PipelineController(ILogger<PipelineController> logger)
+        public PipelineController(ILogger<PipelineController> logger, ITeamService teamService)
         {
             _logger = logger;
+            _teamService = teamService;
         }
 
         [HttpGet]
         [Route("Teams")]
         public ActionResult GetTeam(int teamId, int seasonYear)
         {
+            _teamService.SingleTeamSeasonPipeline(teamId, seasonYear);
+
             return Ok();
         }
 
