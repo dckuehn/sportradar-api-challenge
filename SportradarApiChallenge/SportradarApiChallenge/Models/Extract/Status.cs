e32a5c4096802e7c497de8dd8087e3859443b49f
David Kuehn <dckuehn8@gmail.com>
2021-08-09 17:49:10 -0500

    Adds  missing Status object from Schedule API models

---
+++ SportradarApiChallenge/SportradarApiChallenge/Models/Extract/Status.cs
@@ -0,0 +1,11 @@
+﻿namespace SportradarApiChallenge.Models.Extract
+{
+    public class Status
+    {
+        public string abstractGameState { get; set; }
+        public string codedGameState { get; set; }
+        public string detailedGameState { get; set; } 
+        public string statusCode { get; set; }
+        public bool startTimeTBD { get; set; }
+    }
+}
